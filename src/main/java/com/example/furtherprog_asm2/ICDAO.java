8d2167e92630f325a0101206a0462aecaffaa62b
khoabronze <s3986281@rmit.edu.vn>
2024-05-16 15:34:36 +0700

    Update ICDAO

---
+++ src/main/java/com/example/furtherprog_asm2/ICDAO.java
@@ -0,0 +1,15 @@
+package com.example.furtherprog_asm2;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICDAO {
+    public interface DAO<IC> {
+        List<IC> getAll();
+        Optional<IC> get(String cardNumber);
+        InsuranceCard getOne(String cardNumber);
+        boolean add(IC ic);
+        boolean update(String cardNumber, String cardHolder, String policyOwner, String expirationDate);
+        boolean delete(IC ic);
+    }
+}
