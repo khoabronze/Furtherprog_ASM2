cd01c066f50c1d93b2513d85afb0f6a1869fa928
trian1010 <s3974923@rmit.edu.vn>
2024-05-17 01:30:51 +0700

    navigate between pages

--- src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
+++ src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 public class Navigation_Controller {
     @FXML
     private ImageView claimViewButton;
+    @FXML
+    private ImageView switchStatusButton;
 
     @FXML
     private void handleClaimViewNavigation() throws IOException {
@@ -23,6 +25,21 @@ public class Navigation_Controller {
 
         // Get the current stage
         Stage currentStage = (Stage) claimViewButton.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+
+    @FXML
+    private void handleSwitchStatusNavigation() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Switch-Status-NavBar.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) switchStatusButton.getScene().getWindow();
 
         // Set the new scene on the current stage
         currentStage.setScene(newScene);
