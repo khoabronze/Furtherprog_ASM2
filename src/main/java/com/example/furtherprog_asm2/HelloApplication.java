68a02fc71c69c568c78b3ed97e774166274614f4
trian1010 <s3974923@rmit.edu.vn>
2024-05-16 19:26:35 +0700

    Update frontend for Update-NavBar.fxml

--- src/main/java/com/example/furtherprog_asm2/HelloApplication.java
+++ src/main/java/com/example/furtherprog_asm2/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Db_function db = new Db_function();
         db.connect_to_db();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Claim-Menu-InsuranceSurveyor.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Update-NavBar.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene); // Set the scene to the stage
