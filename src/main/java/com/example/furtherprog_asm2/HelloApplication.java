27ab4362c07ad950e2f8d16c7f2d3af6f348768d
trian1010 <s3974923@rmit.edu.vn>
2024-05-19 17:51:16 +0700

    Test and Debug

--- src/main/java/com/example/furtherprog_asm2/HelloApplication.java
+++ src/main/java/com/example/furtherprog_asm2/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Db_function db = new Db_function();
         db.connect_to_db();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Create-InsuranceSurveyor-Form.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Update-InsuranceManager-Search.fxml"));
       
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
