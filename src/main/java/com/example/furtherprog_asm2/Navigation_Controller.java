ede84a4666b1d603d88d5cda1865c30a7408036a
trian1010 <s3974923@rmit.edu.vn>
2024-05-20 01:20:27 +0700

    render pages

--- src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
+++ src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
@@ -10,6 +10,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Navigation_Controller {
+    // Insurance Surveyor buttons and logos
+    @FXML
+    private ImageView claimInsuranceSurveyorIcon;
+    //
     @FXML
     private ImageView claimViewButton;
     @FXML
@@ -19,6 +23,21 @@ public class Navigation_Controller {
     @FXML
     private ImageView approveDeclineRequestButton;
 
+    // Navigation between pages (Insurance Surveyor)
+    @FXML
+    private void navigateClaimMenuInsuranceSurveyor() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Claim-Menu-Insurance-Surveyor.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) claimInsuranceSurveyorIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
     @FXML
     private void handleClaimViewNavigation() throws IOException {
         // Load the FXML file for the new scene
