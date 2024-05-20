bb0cb266a3fb805da72d7e9abd53ea5f87d1da3f
trian1010 <s3974923@rmit.edu.vn>
2024-05-20 17:31:00 +0700

    Render Pages (Policy Holder Role)

--- src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
+++ src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
@@ -28,6 +28,14 @@ public class Navigation_Controller {
     @FXML
     private ImageView updatePolicyHolderIcon;
     @FXML
+    private ImageView updateDependentIcon;
+    @FXML
+    private ImageView updateClaimIcon;
+    @FXML
+    private ImageView createClaimIcon;
+    @FXML
+    private ImageView deleteClaimIcon;
+    @FXML
     private ImageView claimViewButton;
     @FXML
     private ImageView switchStatusButton;
@@ -170,13 +178,69 @@ public class Navigation_Controller {
     @FXML
     public void navigateUpdatePHSearch() throws IOException {
         // Load the FXML file for the new scene
-        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Update_PH_Search.fxml"));
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Update_PH_search.fxml"));
 
         // Create a new scene
         Scene newScene = new Scene(newSceneParent);
 
         // Get the current stage
         Stage currentStage = (Stage) updatePolicyHolderIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateUpdateDependentSearch() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Update_dependent_search.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) updateDependentIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateUpdateClaimSearch() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Update-Claim-Search.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) updateClaimIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateCreateClaimForm() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Create-Claim-Form.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) createClaimIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    private void navigateDeleteClaimForm() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Delete-Claim-Form.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) deleteClaimIcon.getScene().getWindow();
 
         // Set the new scene on the current stage
         currentStage.setScene(newScene);
