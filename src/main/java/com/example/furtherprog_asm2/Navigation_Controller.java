399deb7386e2fb862f84f7fe36cdc6a78da38409
trian1010 <s3974923@rmit.edu.vn>
2024-05-20 22:45:39 +0700

    render pages

--- src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
+++ src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
@@ -78,6 +78,8 @@ public class Navigation_Controller {
     private ImageView requestViewButton;
     @FXML
     private ImageView approveDeclineRequestButton;
+    @FXML
+    private ImageView calFeeIcon;
 
     // -------------------------------------------------------------------------------------------
     // Insurance Surveyor buttons and logos
@@ -607,6 +609,21 @@ public class Navigation_Controller {
 
         // Get the current stage
         Stage currentStage = (Stage) approveDeclineRequestButton.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+
+    @FXML
+    public void navigatePolicyOwnerCalRate() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("PolicyOwner_CalRate.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) calFeeIcon.getScene().getWindow();
 
         // Set the new scene on the current stage
         currentStage.setScene(newScene);
