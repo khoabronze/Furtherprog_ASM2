4e2f72273c1c8434c089deb8a645893c1ba733f5
trian1010 <s3974923@rmit.edu.vn>
2024-05-20 20:20:00 +0700

    Render pages (Policy Owner Role)

--- src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
+++ src/main/java/com/example/furtherprog_asm2/Navigation_Controller.java
@@ -34,6 +34,10 @@ public class Navigation_Controller {
     @FXML
     private ImageView createClaimIcon;
     @FXML
+    private ImageView createDependentIcon;
+    @FXML
+    private ImageView deleteDependentIcon;
+    @FXML
     private ImageView deleteClaimIcon;
     @FXML
     private ImageView claimViewButton;
@@ -77,6 +81,15 @@ public class Navigation_Controller {
     private ImageView claimPolicyHolderIcon;
     @FXML
     private ImageView homePolicyHolderIcon;
+
+    // -------------------------------------------------------------------------------------------
+    // Policy Owner buttons and logos
+    @FXML
+    private ImageView beneficiariesPolicyOwnerIcon;
+    @FXML
+    private ImageView claimPolicyOwnerIcon;
+    @FXML
+    private ImageView homePolicyOwnerIcon;
 
     // -------------------------------------------------------------------------------------------
     // Navigation between pages (Main)
@@ -235,7 +248,21 @@ public class Navigation_Controller {
         currentStage.setScene(newScene);
     }
     @FXML
-    private void navigateDeleteClaimForm() throws IOException {
+    public void navigateCreateDependentForm() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Create-Dependent-Form.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) createDependentIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateDeleteClaimForm() throws IOException {
         // Load the FXML file for the new scene
         Parent newSceneParent = FXMLLoader.load(getClass().getResource("Delete-Claim-Form.fxml"));
 
@@ -244,6 +271,20 @@ public class Navigation_Controller {
 
         // Get the current stage
         Stage currentStage = (Stage) deleteClaimIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateDeleteDependent() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Delete_dependent.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) deleteDependentIcon.getScene().getWindow();
 
         // Set the new scene on the current stage
         currentStage.setScene(newScene);
@@ -492,6 +533,52 @@ public class Navigation_Controller {
 
         // Get the current stage
         Stage currentStage = (Stage) homePolicyHolderIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+
+    // -------------------------------------------------------------------------------------------
+    // Navigation between pages (Policy Owner)
+    @FXML
+    public void navigateDependentMenuPolicyOwner() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Dependent-Menu-PolicyOwner.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) beneficiariesPolicyOwnerIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+    @FXML
+    public void navigateClaimMenuPolicyOwner() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Claim-Menu-PolicyOwner.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) claimPolicyOwnerIcon.getScene().getWindow();
+
+        // Set the new scene on the current stage
+        currentStage.setScene(newScene);
+    }
+
+    @FXML
+    public void navigateHomepagePolicyOwner() throws IOException {
+        // Load the FXML file for the new scene
+        Parent newSceneParent = FXMLLoader.load(getClass().getResource("Homepage-PolicyOwner.fxml"));
+
+        // Create a new scene
+        Scene newScene = new Scene(newSceneParent);
+
+        // Get the current stage
+        Stage currentStage = (Stage) homePolicyOwnerIcon.getScene().getWindow();
 
         // Set the new scene on the current stage
         currentStage.setScene(newScene);
